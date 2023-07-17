b93412dc2557d27ff258fdd9859179523a803b06
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-07-17 09:00:23 -0700

    SVG added

--- Library/SVG.js
+++ Library/SVG.js
@@ -1,5 +1,17 @@
 class SVG {
-    content(){
-      this.textElement = ""
-      this.shapeElement = ""
-    }
+  constructor () {
+    this.textElement = ''
+    this.shapeElement = ''
+  }
+  setText (textmessage, color) {
+    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="midddle" fill="${color}">${textmessage}</text>`
+  }
+  setShape (shapeElement) {
+    this.shapeElement = shapeElement
+  }
+  render () {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`
+  }
+}
+
+module.exports = SVG
